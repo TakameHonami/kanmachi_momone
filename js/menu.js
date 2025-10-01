31fe52f885ca934b2faed8e33bd090119ded2a06
TakameHonami <140996050+TakameHonami@users.noreply.github.com>
2025-10-01 17:22:56 +0900

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+const menuToggle = document.getElementById('menu-toggle');
+const navMenu = document.querySelector('.main-nav');
+
+menuToggle.addEventListener('click', () => {
+  navMenu.classList.toggle('show');
+});
