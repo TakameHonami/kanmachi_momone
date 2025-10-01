5ba41a64080254a2342ef61b8390e804ece93b5a
TakameHonami <140996050+TakameHonami@users.noreply.github.com>
2025-10-01 17:22:23 +0900

    Delete js directory

--- js/menu.js
+++
@@ -1,7 +0,0 @@
-// ハンバーガーメニュー開閉
-const menuToggle = document.getElementById('menu-toggle');
-const navMenu = document.querySelector('.main-nav'); // ← ul に直接クラスがある
-
-menuToggle.addEventListener('click', () => {
-  navMenu.classList.toggle('show');
-});
